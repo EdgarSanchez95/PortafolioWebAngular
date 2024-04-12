8ac65f7c936f9c59809291969e3d394a01c37739
EdgarSanchez95 <edgar_95_san@hotmail.com>
2024-04-12 00:25:07 -0600

    Se crea el componente Skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import { BannerComponent } from "./components/banner/banner.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { AboutComponent } from "./components/about/about.component";
+import { SkillsComponent } from "./components/skills/skills.component";
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, NavbarComponent, BannerComponent, HeaderComponent, AboutComponent]
+    imports: [RouterOutlet, NavbarComponent, BannerComponent, HeaderComponent, AboutComponent, SkillsComponent]
 })
 export class AppComponent {
   title = 'portafolioWeb';
